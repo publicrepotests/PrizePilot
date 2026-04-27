acf36d043dc87c3b818916b3476822a340f6817b
Shane <shane@local.dev>
2026-04-27 07:29:24 -0500

    Fix analytics

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,5 @@
 import "./globals.css";
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/next";
 export const metadata = {
   title: "PrizePilot",
   description:
@@ -9,7 +9,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
